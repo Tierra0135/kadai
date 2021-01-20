aa71194ced40fbd9bc608df9fa907d9996e0abe6
Tierra0135 <tierra0135@gmail.com>
2021-01-20 11:37:56 +0000

    課題3

--- kadai-03/index1.js
+++ kadai-03/index1.js
@@ -4,7 +4,7 @@ const day = parseInt(process.argv[2])
 if (day % 3 === 0) {
     console.log('あなたは活発な性格です')
 } else if (day % 3 === 1) {
-    console.log('あなたは真面目な性格です')
+    console.log('あなたは神経質な性格です')
 } else {
     console.log('あなたは陽気な性格です')
 }
--- kadai-03/index2.js
+++ kadai-03/index2.js
@@ -1,6 +1,6 @@
 const max = parseInt(process.argv[2])
 
-// 3 の倍数の時に特別なメッセージを出力する
+// 3の倍数の時に特別なメッセージを出力する
 for(let n = 1; n <= max; n = n + 1) {
     if (n % 3 === 0){
         console.log('こんにちは')
